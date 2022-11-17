db84d4dcc0caa257c63cb31969588069cada4ff4
MSI\zuevf <zuev.f.s@gmail.com>
2022-11-17 12:45:29 +0300

    Init Project

---
+++ app/src/main/java/com/example/notesproject/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.notesproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.room_application.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
